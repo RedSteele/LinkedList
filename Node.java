5fb09cf62777d10b98f6f9079c5a5e8a3b5c3559
Sean Steele <steele.sean303@gmail.com>
2013-03-06 12:47:32 -0700

    Created getters and setters as well as constructors for Node.java

--- Node.java
+++ Node.java
@@ -3,6 +3,40 @@
  * 		A member of a linked list. Holds an integer values and a link to another node
  */
 
-class Node{
+public class Node{
 	
+	private int value;
+	private Node next;
+
+	/**
+	 * Constructors
+	 */
+	public Node(int value, Node next){
+		this.value = value;
+		this.next = next;
+	}
+	public Node(){
+		this(0, null);
+	}
+	public Node(int value){
+		this(value, null);
+	}
+
+	/**
+	 * Getters and Setters
+	 */
+
+	public int getValue(){
+		return value;
+	}
+	public void setValue(int value){
+		this.value = value;
+	}
+	public Node getNext(){
+		return next;
+	}
+	public void setNext(Node next){
+		this.next = next;
+	}
+
 }
