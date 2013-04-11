b117b9539ae5d79c5f508da794bcdad71caddba3
Sean Steele <steele.sean303@gmail.com>
2013-04-11 08:45:21 -0600

    Finished a few methods

--- LinkedList.java
+++ LinkedList.java
@@ -1,15 +1,17 @@
 /**
  * Linked List
  */
+import java.util.*;
 
 class LinkedList {
 	private Node head;
-	private int length
+	private int length;
+
 	/**
 	 * constructors
 	 */
 	public LinkedList(){
-		Node head = Node(p1);
+		head = new Node();
 	}
 	/**
 	 * Getters and Setters
@@ -18,16 +20,68 @@ class LinkedList {
 		return head;
 	}
 
+	/**
+	 * push [Given an int, add a new node at the head of the list with this value.]
+	 * @param value [int to be placed in the node value]
+	 */
 	public void push(int value){
-		Node next = Node(value, head.getNode());
-		head.setNode(next);
-		length++;
+		Node next = new Node(value, head.getNext());
+		head.setNext(next);
 	}
 
+	/**
+	 * length [Return the number of nodes in the linked list.]
+	 * @return [returns the length of the linked list]
+	 */
 	public int length(){
+		int length = 0;
+		Node next = head;
+		while(next.getNext()!=null){
+			next = next.getNext();
+			length++;
+		}
 		return length;
 	}
+
+	public void empty(){
+		head = new Node();
+		length = 0;
+	}
+
 	public void buildOneTwoThree(){
+		empty();
+		push(3);
+		push(2);
+		push(1);
+
+	}
+
+	public void print(){
+		if (head == null || head.getNext() == null) {
+			System.out.println("List is empty");
+			return;
+		}
+
+		Node current = head.getNext();
+		while(current != null){
+			System.out.print(current.getValue() + " ");
+			current = current.getNext();
+		}
+	}
+
+	public int getNth(int index){
+		if (index>length()){
+			return -1;
+		}
+		Node next = head;
+		for (int i=0; i!=index; i++) {
+			next = next.getNext();	
+		}
+		return next.getValue();
+	}
+
+	public int pop(){
 		
 	}
+	 
 }
---
+++ Main.java
@@ -0,0 +1,9 @@
+class Main{
+	public static void main(String[] args) {
+		LinkedList a = new LinkedList();
+		a.buildOneTwoThree();
+		a.print();
+		System.out.println(a.getNth(2));
+	}
+}
+
